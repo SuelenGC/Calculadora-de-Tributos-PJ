39fe8a7e2f3b809064f8bf66f3a20c2fc7bdc60b
suelengc <suelengcarvalho@gmail.com>
2012-04-29 05:25:13 -0300

    Inserted shared preference about Tipo Tributacao

--- src/br/com/suelengc/calctributospj/activity/ConfigActivity.java
+++ src/br/com/suelengc/calctributospj/activity/ConfigActivity.java
@@ -1,16 +1,50 @@
 package br.com.suelengc.calctributospj.activity;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 import br.com.suelengc.calctributospj.R;
 
 public class ConfigActivity extends Activity {
+	
+	RadioButton rbLucroPresumido, rbSimples;
+	RadioGroup rgTipoTributacao;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.config);
         setTitle("Calculadora de Tributos PJ - Configurações");
+        
+        rgTipoTributacao = (RadioGroup) findViewById(R.id_config.rgTipoTributacao);
+        rbLucroPresumido = (RadioButton) findViewById(R.id_config.rbLucroPresumido);
+        rbSimples = (RadioButton) findViewById(R.id_config.rbSimples);
+        
+        rgTipoTributacao.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+			
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				if(rbLucroPresumido.getId() == checkedId) {  
+					Toast.makeText(ConfigActivity.this, "Lucro presumido selecionado!", Toast.LENGTH_SHORT).show();
+					setPreference(1);
+					
+			    } else if(rbSimples.getId() == checkedId) {  
+			    	Toast.makeText(ConfigActivity.this, "Simples selecionado!", Toast.LENGTH_SHORT).show();
+			    	setPreference(2);
+			    }  
+			}
+		});
+    }
+    
+    private void setPreference(int tipoTributacao) {
+    	SharedPreferences settings = getSharedPreferences("TIPO_TRIBUTACAO", MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("TipoTributacao", tipoTributacao);
+        editor.commit();
     }
 
+    
 }
--- src/br/com/suelengc/calctributospj/activity/MenuActivity.java
+++ src/br/com/suelengc/calctributospj/activity/MenuActivity.java
@@ -2,8 +2,10 @@ package br.com.suelengc.calctributospj.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import br.com.suelengc.calctributospj.R;
 
 public class MenuActivity extends Activity {
@@ -15,6 +17,8 @@ public class MenuActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu);
 		setTitle("Calculadora de Tributos PJ - Menu");
+		
+		Toast.makeText(MenuActivity.this, "Tipo Tributação: " + getTipoTributacao(), Toast.LENGTH_SHORT).show();
 	}
 	
 	public void CallCalcActivity_ByValuePerHour(View view) {
@@ -28,6 +32,15 @@ public class MenuActivity extends Activity {
 		Intent intent = new Intent(this, CalcActivity.class);
 		intent.putExtra(EXTRA_FORMATO_TELA, "2");
 		startActivity(intent);
+	}
+	
+	private String getTipoTributacao() {
+		SharedPreferences settings = getSharedPreferences("TIPO_TRIBUTACAO", MODE_PRIVATE);
+        int tipoTributacao = settings.getInt("TipoTributacao", 1);
+        if (tipoTributacao == 1) 
+        	return "Lucro Presumido";
+        else 
+        	return "Simples";
 	}
 	
 }
