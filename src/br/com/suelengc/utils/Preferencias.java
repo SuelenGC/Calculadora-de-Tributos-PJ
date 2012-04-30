93cc65c0e7bcc73cc0977b09d039ff20f62db0f2
suelengc <suelengcarvalho@gmail.com>
2012-04-30 03:09:38 -0300

    Created specialized class of get and set preferences

---
+++ src/br/com/suelengc/utils/Preferencias.java
@@ -0,0 +1,38 @@
+package br.com.suelengc.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+
+public class Preferencias {
+	public static final String PREF_FILE_CTPJ = "PREFERENCES_CTPJ";
+	
+    public static int getPreferenciaValor(Context c, String item) {
+    	SharedPreferences settings = c.getSharedPreferences(PREF_FILE_CTPJ, Context.MODE_PRIVATE);
+        int tipoTributacao = settings.getInt(item, 1);
+        return tipoTributacao;
+	}
+
+    public static String getPreferenciaDescricao(Context c, String item) {
+    	SharedPreferences settings = c.getSharedPreferences(PREF_FILE_CTPJ, Context.MODE_PRIVATE);
+        int tipoTributacao = settings.getInt(item, 1);
+        
+        String descricao = "";
+        
+        if (tipoTributacao == 1) {
+        	descricao = "Lucro Presumido";
+        } else if (tipoTributacao == 2) {
+        	descricao = "Simples Nacional";
+        }
+        return descricao;
+	}
+    
+    public static void setPreferencia(Context c, String item, int valorConfiguracao) {
+    	SharedPreferences settings = c.getSharedPreferences(PREF_FILE_CTPJ, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(item, valorConfiguracao);
+        editor.commit();
+    }
+    
+}
+
