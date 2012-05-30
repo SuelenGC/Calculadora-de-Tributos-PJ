ffe015f51afd7f4415ec9b113c41c1e6d3313d71
suelengc <suelengcarvalho@gmail.com>
2012-05-29 21:44:54 -0300

    Add test project

---
+++ CalculadoraImpostoPJ-Test/src/br/com/suelengc/calctributospj/controller/test/NotaFiscalControllerTest.java
@@ -0,0 +1,28 @@
+package br.com.suelengc.calctributospj.controller.test;
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+import br.com.suelengc.calctributospj.controller.*;
+
+public class NotaFiscalControllerTest extends AndroidTestCase {
+
+	public void testNotaFiscalAPartirDoValorBruto() {
+		double valorBruto = 5000;
+		
+		NotaFiscalController notaFiscalController = new NotaFiscalController(valorBruto);
+		
+		Assert.assertNotNull(notaFiscalController);
+		Assert.assertEquals(notaFiscalController.getNotaFiscal().getValorBruto(), 5000.0);
+	}
+	
+	public void testNotaFiscalAPartirDoValorPorHoraEQtdeDeHoras() {
+		double valorPorHora = 55;
+		double qtdeHoras = 168;
+				
+		NotaFiscalController notaFiscalController = new NotaFiscalController(valorPorHora, qtdeHoras);
+		
+		Assert.assertNotNull(notaFiscalController);
+		Assert.assertEquals(notaFiscalController.getNotaFiscal().getValorBruto(), 9240.0);
+	}
+	
+}
