aecaed2663633a12e453c1f5595cb5803b4bbb4b
suelengc <suelengcarvalho@gmail.com>
2012-06-05 01:11:28 -0300

    Adjust de tests

--- Calculadora-de-Tributos-PJ-Test/src/br/com/suelengc/calctributospj/controller/test/NotaFiscalControllerTest.java
+++ Calculadora-de-Tributos-PJ-Test/src/br/com/suelengc/calctributospj/controller/test/NotaFiscalControllerTest.java
@@ -9,7 +9,7 @@ public class NotaFiscalControllerTest extends AndroidTestCase {
 	public void testNotaFiscalAPartirDoValorBruto() {
 		double valorBruto = 5000;
 		
-		NotaFiscalController notaFiscalController = new NotaFiscalController(valorBruto);
+		NotaFiscalController notaFiscalController = new NotaFiscalController(valorBruto, 1);
 		
 		Assert.assertNotNull(notaFiscalController);
 		Assert.assertEquals(notaFiscalController.getNotaFiscal().getValorBruto(), 5000.0);
@@ -19,7 +19,7 @@ public class NotaFiscalControllerTest extends AndroidTestCase {
 		double valorPorHora = 55;
 		double qtdeHoras = 168;
 				
-		NotaFiscalController notaFiscalController = new NotaFiscalController(valorPorHora, qtdeHoras);
+		NotaFiscalController notaFiscalController = new NotaFiscalController(valorPorHora, qtdeHoras, 1);
 		
 		Assert.assertNotNull(notaFiscalController);
 		Assert.assertEquals(notaFiscalController.getNotaFiscal().getValorBruto(), 9240.0);
