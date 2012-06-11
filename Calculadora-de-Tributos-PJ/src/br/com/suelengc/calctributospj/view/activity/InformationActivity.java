5346888412d04e72a3f4ecb7e32ae08da0a363a7
suelengc <suelengcarvalho@gmail.com>
2012-06-11 00:38:28 -0300

    Changed the title of 'Sobre' to 'Information' and now get the texts from strings_information.xml

--- Calculadora-de-Tributos-PJ/src/br/com/suelengc/calctributospj/view/activity/InformationActivity.java
+++ Calculadora-de-Tributos-PJ/src/br/com/suelengc/calctributospj/view/activity/InformationActivity.java
@@ -4,14 +4,14 @@ import br.com.suelengc.calctributospj.R;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SobreActivity extends Activity {
+public class InformationActivity extends Activity {
 
 		    
 	    @Override
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setTitle("Calculadora de Tributos PJ - Sobre");
-	        setContentView(R.layout.sobre);
+	        setContentView(R.layout.information);
 	    }
 
 	
