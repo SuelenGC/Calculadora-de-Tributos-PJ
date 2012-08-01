9a496cd080a04cef9e3b1e58c65a76e9cc7c5e29
suelengc <suelengcarvalho@gmail.com>
2012-08-01 02:09:49 -0300

    Changed title of calc screen

--- Calculadora-de-Tributos-PJ/src/br/com/suelengc/calctributospj/view/activity/CalculadoraActivity.java
+++ Calculadora-de-Tributos-PJ/src/br/com/suelengc/calctributospj/view/activity/CalculadoraActivity.java
@@ -13,6 +13,8 @@ public class CalculadoraActivity extends SherlockFragmentActivity {
 	    super.onCreate(savedInstanceState);
         setContentView(R.layout.calculadora);
 
+        setTitle("Calculo valor/hora");
+        
         GradientDrawable bg = (GradientDrawable) getResources().getDrawable(R.drawable.bg_gradient);
         getSupportActionBar().setBackgroundDrawable(bg);
 	}
