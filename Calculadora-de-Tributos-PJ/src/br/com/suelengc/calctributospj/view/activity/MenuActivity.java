d7d202270b1dc197b49e2341d512b6dab65d54e2
SuelenGC <suelengcarvalho@gmail.com>
2012-08-20 22:55:59 -0300

    Removed comment

--- Calculadora-de-Tributos-PJ/src/br/com/suelengc/calctributospj/view/activity/MenuActivity.java
+++ Calculadora-de-Tributos-PJ/src/br/com/suelengc/calctributospj/view/activity/MenuActivity.java
@@ -31,9 +31,6 @@ public class MenuActivity extends SherlockActivity {
 	
 	public void CallCalcActivity_ByValuePerHour(View view) {
 		Intent intent = new Intent(this, CalculadoraActivity.class);
-
-		//Log.d("CalculadoraActivity-Log", "TipoBaseCalculo Menu: " + TipoBaseCalculo.VALOR_HORA.toString());
-		
 		intent.putExtra(EXTRA_FORMATO_TELA, TipoBaseCalculo.VALOR_HORA.toString());
 		startActivity(intent);
 		
