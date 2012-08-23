1eb934f2240d034da57cd005e2ba526fc2442e42
SuelenGC <suelengcarvalho@gmail.com>
2012-08-23 00:09:11 -0300

    Created a factory to set the right invoice type according with preferences

---
+++ Calculadora-de-Tributos-PJ/src/br/com/suelengc/calctributospj/controller/TributoFactory.java
@@ -0,0 +1,23 @@
+package br.com.suelengc.calctributospj.controller;
+
+import br.com.suelengc.calctributospj.domain.TipoTributacao;
+import br.com.suelengc.calctributospj.model.LucroPresumido;
+import br.com.suelengc.calctributospj.model.SimplesNacional;
+import br.com.suelengc.calctributospj.model.Tributo;
+import br.com.suelengc.calctributospj.preference.Preferencias;
+import br.com.suelengc.calctributospj.preference.PreferenciasCalculo;
+
+public class TributoFactory {
+
+	public Tributo Create(PreferenciasCalculo preferencias) {
+		Tributo tributo = null;
+		
+		if (preferencias.getTipoTributacao().equals(TipoTributacao.LUCRO_PRESUMIDO)) {
+			tributo = new LucroPresumido(preferencias.getPercentIRPJ());
+		} else if (preferencias.getTipoTributacao().equals(TipoTributacao.SIMPLES_NACIONAL)) {
+			tributo = new SimplesNacional();
+		}
+		return tributo;
+	}
+
+}
