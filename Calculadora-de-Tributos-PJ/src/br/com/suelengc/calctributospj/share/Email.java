890593cc6153ef1c57bd3d6d2e6dc13eb99348fc
SuelenGC <suelengcarvalho@gmail.com>
2012-09-15 16:31:15 -0300

    Encapsuled the creation about intent email

---
+++ Calculadora-de-Tributos-PJ/src/br/com/suelengc/calctributospj/share/Email.java
@@ -0,0 +1,17 @@
+package br.com.suelengc.calctributospj.share;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class Email {
+
+	
+	public void sendEmail(Context context) {
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("message/rfc822");
+		String[] emails = {"suelengcarvalho@gmail.com"};
+		intent.putExtra(Intent.EXTRA_EMAIL, emails);
+		intent.putExtra(Intent.EXTRA_SUBJECT, "Calculadora Tributos PJ - Feedback");
+		context.startActivity(Intent.createChooser(intent, "Selecione a aplicação de email:"));
+	}
+}
