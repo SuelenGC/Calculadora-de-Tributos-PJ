2d604a1b3a6ef57acbf2863b23b2d15da5d511e7
SuelenGC <suelengcarvalho@gmail.com>
2012-09-15 12:06:14 -0300

    Separeted legible menu itens id from code of Activities

---
+++ Calculadora-de-Tributos-PJ/src/br/com/suelengc/calctributospj/view/menu/MyMenu.java
@@ -0,0 +1,10 @@
+package br.com.suelengc.calctributospj.view.menu;
+
+import br.com.suelengc.calctributospj.R;
+
+public class MyMenu {
+	public static final int HOME = android.R.id.home;
+	public static final int SETTINGS = R.id_menu.Settings;
+	public static final int ABOUT = R.id_menu.About;
+
+}
